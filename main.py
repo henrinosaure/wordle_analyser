7ae4e88c3de4643578a8dd9068a7c299c617956f
Henry Cloutier <heclo33@ulaval.ca>
2025-11-11 20:04:05 -0500

    some updates to main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import wordslist,wordle_functions
 from wordle_functions import find_possible_words
 from wordslist import filtered_list
+from optimal_finder import best_word_finder
 
 
 for i in range(6):
@@ -13,6 +14,11 @@ for i in range(6):
         remaining_words = find_possible_words(attempt1,colors1,filtered_list)
     else:
         remaining_words = find_possible_words(attempt1, colors1, remaining_words)
-    print(f"The {len(remaining_words)} possible words are: {remaining_words}")
-
+    print(f"The {len(remaining_words)} possible words are: ")
+    print(remaining_words)
+    best_word = best_word_finder(remaining_words)
+    if best_word=={"AAHED":0.0}:
+        print(f"The best word to play is {remaining_words[0]}")
+    else:
+        print(f"the best word to play is {best_word_finder(remaining_words)}")
 
